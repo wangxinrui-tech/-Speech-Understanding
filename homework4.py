dc3d3d2477e685dd6059dae2d4c1be4b062e8ca7
wangxinrui-tech <wxr991011@outlook.com>
2025-06-13 15:53:54 +0900

    Create homework4.py

---
+++ homework4.py
@@ -0,0 +1,7 @@
+def list_to_dict(input_list):
+    '''
+    This function should return a dictionary in which each element of 
+    `input_list` is a value, and the corresponding key is the numerical 
+    index of that element in `input_list`. 
+    '''
+    return {index: value for index, value in enumerate(input_list)}
