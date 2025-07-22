d78789ba29b1a6efa553bf710ac003073e7090bb
wangxinrui-tech <wxr991011@outlook.com>
2025-07-22 15:50:11 +0900

    Create homework12.py

---
+++ homework12.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+
+def synthesize(text, lang, filename):
+    '''
+    Use gtts.gTTs(text=text, lang=lang) to synthesize speech, then write it to filename.
+    
+    @params:
+    text (str) - the text you want to synthesize
+    lang (str) - the language in which you want to synthesize it
+    filename (str) - the filename in which it should be saved
+    '''
+    tts = gTTS(text=text, lang=lang)
+    tts.save(filename)
